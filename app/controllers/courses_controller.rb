649fb54476767de2f52421e56ed7bfbf72c58aab
ashishmainalee <ashishmainalee@gmail.com>
2019-06-28 09:26:12 +0545

    course fix

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,13 +1,20 @@
 class CoursesController < ApplicationController
 
+	before_action :set_course, only: [:show, :edit, :update]
 	skip_before_action :require_user
 
 	def index
-
 	end
 
 	def new
+	end
 
+	def show
+	end
+
+	private
+	def set_course
+		@course = Course.find(params[:id])
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'courses#index'
-  get 'courses/new', to: 'courses#new'
+  # get 'courses/new', to: 'courses#new'
+  # get 'courses/show/:id', to: 'courses#show'
+  resources :courses
   get 'about', to: 'pages#about'
   resources :students, except: [:destroy]
   get 'login', to: 'logins#new'
